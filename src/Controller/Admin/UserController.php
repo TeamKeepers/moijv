1c935109a833da67e511033b3b6ae81846c158f5
TeamKeepers <team@keepizi.com>
2018-02-07 11:42:59 +0100

    Link between users and details user, routing name update

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -9,7 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class UserController extends Controller
 {
     /**
-     * @Route("admin/user", name="List_user")
+     * @Route("admin/user", name="list_user")
      */
     public function getList(UserRepository $userRepo) // équivaut à faire un $userRepo (objet, instanciation de la class) = new UserRepository
     {
@@ -23,15 +24,22 @@ class UserController extends Controller
     }
     
     /**
-     * @Route("/admin/user/{id}")
+     * @Route("/admin/user/{id}", name="user_details")
      */
-    public function details(UserRepository $userRepo, $id) {
-        $user = $userRepo->find($id);
-        
-    return $this->render('admin/details_user.html.twig', [
-        'user' => $user
-    ]);
-    
+    public function details(User $user) {
+        return $this->render('admin/details_user.html.twig', [
+            'user' => $user
+        ]);
     }
+    // NE PAS OUBLIER DE METTRE EN USE LE CHEMIN VERS ENTITY
+//  function ci-dessus équivaut à l'écriture suivante sous symfony précédent au 4 :
+//    public function details(UserRepository $userRepo, $id) {
+//        $user = $userRepo->find($id);
+//        
+//    return $this->render('admin/details_user.html.twig', [
+//        'user' => $user
+//    ]);
+//    
+//    }
     
 }
